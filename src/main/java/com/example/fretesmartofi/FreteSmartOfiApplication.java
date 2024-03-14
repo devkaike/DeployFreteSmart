8977f65221f2cc1160e6e0273461664b68932615
CAIKYMAJ <caikymagalhaes@gmail.com>
2024-03-14 19:42:56 -0300

    first commit

---
+++ src/main/java/com/example/fretesmartofi/FreteSmartOfiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fretesmartofi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteSmartOfiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreteSmartOfiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fretesmartofi/FreteSmartOfiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fretesmartofi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteSmartOfiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
