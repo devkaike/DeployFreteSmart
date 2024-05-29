c9caada4878e772bfd99ddd523c7c7b91a6eeb16
kaike silva rocha <skaike718@gmail.com>
2024-05-29 19:48:59 -0300

    mudanças no motorista

---
+++ src/main/java/com/example/fretesmartofi/repository/CaminhaoRepository.java
@@ -0,0 +1,7 @@
+package com.example.fretesmartofi.repository;
+
+import com.example.fretesmartofi.model.Caminhao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CaminhaoRepository extends JpaRepository<Caminhao, Long> {
+}
--- src/main/java/com/example/fretesmartofi/service/MotoristaService.java
+++ src/main/java/com/example/fretesmartofi/service/MotoristaService.java
@@ -1,7 +1,14 @@
 package com.example.fretesmartofi.service;
 
+import com.example.fretesmartofi.model.Caminhao;
+import com.example.fretesmartofi.model.Cidade;
 import com.example.fretesmartofi.model.Motorista;
+import com.example.fretesmartofi.model.Usuario;
+import com.example.fretesmartofi.repository.CaminhaoRepository;
+import com.example.fretesmartofi.repository.CidadeRepository;
 import com.example.fretesmartofi.repository.MotoristaRepository;
+import com.example.fretesmartofi.repository.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +20,15 @@ public class MotoristaService {
     @Autowired
     private MotoristaRepository motoristaRepository;
 
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private CaminhaoRepository caminhaoRepository;
+
     public List<Motorista> getAllMotoristas() {
         return motoristaRepository.findAll();
     }
@@ -22,6 +38,24 @@ public class MotoristaService {
     }
 
     public Motorista saveMotorista(Motorista motorista) {
+        if (motorista.getCaminhao() != null) {
+            Caminhao caminhao = caminhaoRepository.findById(motorista.getCaminhao().getId())
+                    .orElseThrow(() -> new EntityNotFoundException("Caminhao not found"));
+            motorista.setCaminhao(caminhao);
+        }
+
+        if (motorista.getCidade() != null) {
+            Cidade cidade = cidadeRepository.findById(motorista.getCidade().getId())
+                    .orElseThrow(() -> new EntityNotFoundException("Cidade not found"));
+            motorista.setCidade(cidade);
+        }
+
+        if (motorista.getUsuario() != null) {
+            Usuario usuario = usuarioRepository.findById(motorista.getUsuario().getId())
+                    .orElseThrow(() -> new EntityNotFoundException("Usuario not found"));
+            motorista.setUsuario(usuario);
+        }
+
         return motoristaRepository.save(motorista);
     }
 
