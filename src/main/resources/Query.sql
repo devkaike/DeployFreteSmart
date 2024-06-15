5b5d6a819cd1dee7b62f80325fa839b425fc36a1
emartins-dev <edmilsonmartins.dvg@gmail.com>
2024-06-15 08:58:35 -0300

    Ajustes na Query

--- src/main/resources/Query.sql
+++ src/main/resources/Query.sql
@@ -1,25 +1,25 @@
 -- Inserindo Usuários
-INSERT INTO usuarios (login, senha)
+INSERT INTO usuarios (login, senha, role)
 VALUES 
-('joaosilva', 'senhaSegura123'),
-('mariaoliveira', 'senhaSegura456'),
-('carlosmedeiros', 'senhaSegura789'),
-('anacarvalho', 'senhaSegura101'),
-('pedroramos', 'senhaSegura202'),
-('luizafreitas', 'senhaSegura303'),
-('robertosouza', 'senhaSegura404'),
-('fernandalima', 'senhaSegura505'),
-('julianacosta', 'senhaSegura606'),
-('gustavopereira', 'senhaSegura707'),
-('mariasantos', 'senhaSegura808'),
-('anacosta', 'senhaSegura111'),
-('pedroalves', 'senhaSegura333'),
-('carlamelo', 'senhaSegura444'),
-('lucasferreira', 'senhaSegura555'),
-('fernandaoliveira', 'senhaSegura666'),
-('rafaellima', 'senhaSegura777'),
-('beatrizbarros', 'senhaSegura888'),
-('tiagoribeiro', 'senhaSegura999');
+('joaosilva', 'senhaSegura123', '0'),
+('mariaoliveira', 'senhaSegura456', '0'),
+('carlosmedeiros', 'senhaSegura789', '0'),
+('anacarvalho', 'senhaSegura101', '2'),
+('pedroramos', 'senhaSegura202', '2'),
+('luizafreitas', 'senhaSegura303', '2'),
+('robertosouza', 'senhaSegura404', '2'),
+('fernandalima', 'senhaSegura505', '2'),
+('julianacosta', 'senhaSegura606', '2'),
+('gustavopereira', 'senhaSegura707', '2'),
+('mariasantos', 'senhaSegura808', '1'),
+('anacosta', 'senhaSegura111', '1'),
+('pedroalves', 'senhaSegura333', '1'),
+('carlamelo', 'senhaSegura444', '1'),
+('lucasferreira', 'senhaSegura555', '1'),
+('fernandaoliveira', 'senhaSegura666', '1'),
+('rafaellima', 'senhaSegura777', '1'),
+('beatrizbarros', 'senhaSegura888', '1'),
+('tiagoribeiro', 'senhaSegura999', '1');
 
 -- Inserindo Administradores
 INSERT INTO administradores (`cpf`, `email`, `nome`, `sobrenome`, `celular`, `usuario_id`, `cep`, `bairro`, `cidade_id`, `complemento`, `endereco`)
@@ -54,20 +54,19 @@ VALUES
 ('BCD7890', '12345678910', '0BWZZZ377VT004250', 'Hyundai', '2014', 'Vermelho', 11000.0, 2, 'FRIGORIFICO', 'MT');
 
 -- Inserindo Motoristas
-INSERT INTO motoristas (nome, sobrenome, data_nascimento, cpf, cnh, tipo_cnh, celular, email, usuario_id, cep, bairro, cidade_id, complemento, endereco) 
+INSERT INTO motoristas (nome, sobrenome, data_nascimento, cpf, cnh, tipo_cnh, celular, email, usuario_id, cep, bairro, cidade_id, complemento, endereco, ativo) 
 VALUES 
-('Maria', 'Santos', '1988-04-10', '98765432100', '12345678900', 'D', '62987654321', 'maria.santos@example.com', 11, '12345-678', 'Centro', 1, 'Apto 101', 'Rua das Flores, 123');
+('Maria', 'Santos', '1988-04-10', '98765432100', '12345678900', 'D', '62987654321', 'maria.santos@example.com', 11, '12345-678', 'Centro', 1, 'Apto 101', 'Rua das Flores, 123', false);
 
-
-INSERT INTO motoristas (nome, sobrenome, data_nascimento, cpf, cnh, tipo_cnh, celular, email, usuario_id, caminhao_id, cep, bairro, cidade_id, complemento, endereco) 
+INSERT INTO motoristas (nome, sobrenome, data_nascimento, cpf, cnh, tipo_cnh, celular, email, usuario_id, caminhao_id, cep, bairro, cidade_id, complemento, endereco, ativo) 
 VALUES 
-('Ana', 'Costa', '1990-07-22', '12345678901', '98765432101', 'C', '62912345678', 'ana.costa@example.com', 12, 2, '12345-678', 'Centro', 1, 'Apto 101', 'Rua das Flores, 123'),
-('Pedro', 'Alves', '1975-01-30', '23456789012', '87654321012', 'E', '62923456789', 'pedro.alves@example.com', 13, 3, '23456-789', 'Vila Nova', 2, 'Casa 202', 'Avenida Principal, 456'),
-('Carla', 'Melo', '1982-03-05', '34567890123', '76543210987', 'B', '62934567890', 'carla.melo@example.com', 14, 4, '34567-890', 'Jardim das Flores', 3, 'Bloco B, Ap. 303', 'Rua das Palmeiras, 789'),
-('Lucas', 'Ferreira', '1995-08-12', '45678901234', '65432109876', 'D', '62945678901', 'lucas.ferreira@example.com', 15, 5, '45678-901', 'Alameda dos Sonhos', 4, 'Casa 404', 'Travessa das Estrelas, 101'),
-('Fernanda', 'Oliveira', '1983-09-25', '56789012345', '54321098765', 'C', '62956789012', 'fernanda.oliveira@example.com', 16, 6, '56789-012', 'Loteamento Feliz', 5, 'Apto 505', 'Avenida dos Girassóis, 202'),
-('Rafael', 'Lima', '1978-12-10', '67890123456', '43210987654', 'E', '62967890123', 'rafael.lima@example.com', 17, 7, '67890-123', 'Bairro Industrial', 6, 'Casa 606', 'Rua das Indústrias, 303'),
-('Beatriz', 'Barros', '1992-11-17', '78901234567', '32109876543', 'B', '62978901234', 'beatriz.barros@example.com', 18, 8, '78901-234', 'Parque Residencial', 7, 'Bloco C, Ap. 707', 'Avenida dos Ipês, 404'),
-('Tiago', 'Ribeiro', '1987-02-28', '89012345678', '21098765432', 'D', '62989012345', 'tiago.ribeiro@example.com', 19, 9, '89012-345', 'Condomínio Primavera', 8, 'Casa 808', 'Rua das Orquídeas, 505');
+('Ana', 'Costa', '1990-07-22', '12345678901', '98765432101', 'C', '62912345678', 'ana.costa@example.com', 12, 2, '12345-678', 'Centro', 1, 'Apto 101', 'Rua das Flores, 123', true),
+('Pedro', 'Alves', '1975-01-30', '23456789012', '87654321012', 'E', '62923456789', 'pedro.alves@example.com', 13, 3, '23456-789', 'Vila Nova', 2, 'Casa 202', 'Avenida Principal, 456', true),
+('Carla', 'Melo', '1982-03-05', '34567890123', '76543210987', 'B', '62934567890', 'carla.melo@example.com', 14, 4, '34567-890', 'Jardim das Flores', 3, 'Bloco B, Ap. 303', 'Rua das Palmeiras, 789', true),
+('Lucas', 'Ferreira', '1995-08-12', '45678901234', '65432109876', 'D', '62945678901', 'lucas.ferreira@example.com', 15, 5, '45678-901', 'Alameda dos Sonhos', 4, 'Casa 404', 'Travessa das Estrelas, 101', true),
+('Fernanda', 'Oliveira', '1983-09-25', '56789012345', '54321098765', 'C', '62956789012', 'fernanda.oliveira@example.com', 16, 6, '56789-012', 'Loteamento Feliz', 5, 'Apto 505', 'Avenida dos Girassóis, 202', true),
+('Rafael', 'Lima', '1978-12-10', '67890123456', '43210987654', 'E', '62967890123', 'rafael.lima@example.com', 17, 7, '67890-123', 'Bairro Industrial', 6, 'Casa 606', 'Rua das Indústrias, 303', true),
+('Beatriz', 'Barros', '1992-11-17', '78901234567', '32109876543', 'B', '62978901234', 'beatriz.barros@example.com', 18, 8, '78901-234', 'Parque Residencial', 7, 'Bloco C, Ap. 707', 'Avenida dos Ipês, 404', true),
+('Tiago', 'Ribeiro', '1987-02-28', '89012345678', '21098765432', 'D', '62989012345', 'tiago.ribeiro@example.com', 19, 9, '89012-345', 'Condomínio Primavera', 8, 'Casa 808', 'Rua das Orquídeas, 505', true);
 
 
